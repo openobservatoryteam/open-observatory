1af2d8b0088fabda08fe9283a9da2cc12b518649
Thomas Hamelin <thomashamelin27@outlook.de>
2023-01-30 14:04:31 +0100

    define theme typography

---
+++ frontend/src/theme.ts
@@ -0,0 +1,43 @@
+import { createTheme } from '@mui/material/styles';
+import PoppinsRegular from './font/Poppins-Regular.ttf';
+import PoppinsSemiBold from './font/Poppins-SemiBold.ttf';
+
+const theme = createTheme({
+  palette: {
+    
+  },
+});
+
+theme.typography.h1 = {
+  fontFamily: 'PoppinsRegular, Arial',
+  fontSize: 35,
+  [theme.breakpoints.down("md")] : {
+    fontSize: 16,
+  }
+}
+
+theme.typography.h2 = {
+  fontFamily: 'PoppinsSemiRegular, Arial',
+  fontSize: 26,
+  [theme.breakpoints.down("md")]: {
+    fontSize: 16,
+  }
+}
+
+theme.typography.body1 = {
+  fontFamily: 'PoppinsRegular, Arial',
+  fontSize: 20,
+  [theme.breakpoints.down("md")]: {
+    fontSize: 16,
+  }
+}
+
+theme.typography.caption = {
+  fontFamily: 'PoppinsRegular, Arial',
+  fontSize: 16,
+  [theme.breakpoints.down("md")]: {
+    fontSize: 13,
+  }
+}
+
+export default theme;
