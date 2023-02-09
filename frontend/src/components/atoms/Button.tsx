d424e621916e5e20b434f9bd71c2a4e33a886a4c
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-02-09 18:04:21 +0100

    frontend: add color variants to Button

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -2,20 +2,31 @@ import clsx from 'clsx';
 import { ComponentPropsWithoutRef, ElementType, ReactNode, useRef } from 'react';
 import { AriaButtonProps, useButton, useFocusVisible } from 'react-aria';
 
-const styles = (isPressed = false) =>
+type ButtonColor = 'lightGray' | 'darkGray' | 'red';
+
+const styles = (color: ButtonColor, isPressed = false) =>
   clsx(
-    'bg-gray-300 px-8 py-2.5 rounded-3xl text-black',
+    'px-8 py-2.5 rounded-3xl',
     'disabled:cursor-not-allowed disabled:brightness-75',
     isPressed && 'brightness-90',
+    color === 'lightGray'
+      ? 'bg-gray-300 text-black'
+      : color === 'darkGray'
+      ? 'bg-[#333C47] text-white'
+      : color === 'red'
+      ? 'bg-[#880000] text-white'
+      : '',
   );
 
 type ButtonProps<C extends ElementType> =
-  | { as?: C; children: ReactNode; unstyled?: boolean } & AriaButtonProps<C> & ComponentPropsWithoutRef<C>;
+  | { as?: C; children: ReactNode; color?: ButtonColor; unstyled?: boolean } & AriaButtonProps<C> &
+      ComponentPropsWithoutRef<C>;
 
 export const Button = <C extends ElementType>({
   as,
   className,
   children,
+  color = 'lightGray',
   onPress,
   unstyled,
   ...props
@@ -27,7 +38,7 @@ export const Button = <C extends ElementType>({
   return (
     <Component
       className={clsx(
-        !unstyled && styles(isPressed),
+        !unstyled && styles(color, isPressed),
         'outline-none',
         isFocusVisible && 'focus:ring-4',
         'block',
