ef50eaaae2f4357093c218c78be96e7b6e38a60d
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-02-10 08:01:57 +0100

    frontend: fix homepage button text sizes

--- frontend/src/routes/HomePage.tsx
+++ frontend/src/routes/HomePage.tsx
@@ -10,13 +10,18 @@ function HomePage() {
       <div className="flex justify-around items-center my-5 md:my-7">
         <Button
           as={Link}
-          className="w-40 sm:text-[10px] md:w-96 h-20 text-[14px] md:text-[20px]"
+          className="h-20 w-40 md:w-96 text-[10px] sm:text-[14px] md:text-[20px]"
           color="darkGray"
           to="/login"
         >
           Nouvelle observation
         </Button>
-        <Button as={Link} className="w-40 md:w-96 h-20 text-[14px] md:text-[20px]" color="darkGray" to="/login">
+        <Button
+          as={Link}
+          className="h-20 w-40 md:w-96 text-[10px] sm:text-[14px] md:text-[20px]"
+          color="darkGray"
+          to="/login"
+        >
           Se connecter
         </Button>
       </div>
