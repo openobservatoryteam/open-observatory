7ec6f728e3afce0312ee95693ffc0975b5122726
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-02-10 09:15:42 +0100

    frontend: ship leaflet styles on Map only

--- frontend/src/components/templates/Map.tsx
+++ frontend/src/components/templates/Map.tsx
@@ -1,17 +1,25 @@
+import leafletStylesheet from 'leaflet/dist/leaflet.css?inline';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 import { CurrentPosition } from '@/components';
+import { Helmet } from 'react-helmet';
 
 export function Map() {
+  console.log(leafletStylesheet);
   return (
-    <MapContainer
-      className="h-[calc(100vh-19em)] md:h-[calc(100vh-21.6em)] relative top-4"
-      center={[48.866667, 2.333333]}
-      zoom={10}
-      attributionControl={false}
-    >
-      <TileLayer url="https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" />
-      <CurrentPosition />
-    </MapContainer>
+    <>
+      <Helmet>
+        <style type="text/css">{leafletStylesheet}</style>
+      </Helmet>
+      <MapContainer
+        attributionControl={false}
+        center={[48.866667, 2.333333]}
+        className="h-[calc(100vh-19em)] md:h-[calc(100vh-21.6em)] relative top-4"
+        zoom={10}
+      >
+        <TileLayer url="https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png" />
+        <CurrentPosition />
+      </MapContainer>
+    </>
   );
 }
