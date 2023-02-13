3d6ae7c3d6162d840a39905927af7d9127f87f2d
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-02-13 14:37:29 +0100

    frontend: no more useless clsx + alphabetical sort

--- frontend/src/routes/ObservationPage.tsx
+++ frontend/src/routes/ObservationPage.tsx
@@ -16,72 +16,72 @@ export default function ObservationPage(): JSX.Element {
   return (
     <div className="md:flex md:w-full">
       <div className="w-full">
-        <div className={clsx('w-full h-1/3 relative', 'md:h-[50vh]')}>
+        <div className="h-1/3 md:h-[50vh] relative w-full">
           {expired && (
-            <Text as="span" color="black" centered className="absolute top-5 left-5 py-2 px-5 rounded-[45px] bg-white">
+            <Text as="span" color="black" centered className="absolute bg-white left-5 py-2 px-5 rounded-[45px] top-5">
               Expirée
             </Text>
           )}
           {isUser && (
             <Button
+              className="absolute bg-white flex h-14 md:h-16 justify-center items-center right-2 rounded-[45px] top-4 w-14 md:w-16"
               unstyled
-              className="bg-white absolute top-4 right-2 w-14 md:w-16 md:h-16 h-14 flex justify-center items-center rounded-[45px]"
             >
               <FontAwesomeIcon icon={faPen} size="xl" />
             </Button>
           )}
           <img src={celest} alt="image de l'observation" className="h-full w-full" />
-          <div className={clsx('absolute bottom-1 right-2  flex-col justify-around items-center')}>
-            <div className="w-10 flex justify-center items-center">
+          <div className="absolute bottom-1 flex-col justify-around items-center right-2">
+            <div className="flex justify-center items-center w-10">
               <Button unstyled>
                 <FontAwesomeIcon icon={faArrowUp} size="2xl" color="white" />
               </Button>
             </div>
-            <Text as="p" color="white" centered className={clsx('w-10 py-1')}>
+            <Text as="p" centered className="py-1 w-10" color="white">
               10
             </Text>
-            <div className="w-10 flex justify-center items-center">
+            <div className="flex justify-center items-center w-10">
               <Button unstyled>
                 <FontAwesomeIcon icon={faArrowDown} size="2xl" color="grey" />
               </Button>
             </div>
           </div>
         </div>
-        <div className={clsx('w-full h-1/4 bg-slate-500 pb-5', 'md:h-[50vh]')}>
-          <div className="pt-4 pl-3 flex justify-between">
+        <div className="bg-slate-500 h-1/4 pb-5 md:h-[50vh] w-full">
+          <div className="flex justify-between pl-3 pt-4">
             <div className="w-1/2">
               <Text as="h1" bold className="text-lg md:text-2xl">
                 Galaxie Messier
               </Text>
-              <Text as="p" className={clsx('text-xs', 'md:text-base')}>
+              <Text as="p" className="text-xs md:text-base">
                 le 22/11/2022 à 18h44
               </Text>
             </div>
             <div className="flex items-center pr-4 md:pr-10">
-              <img src={iconUser} className={clsx('w-10 md:w-12')} />
-              <Text as="span" color="white" className="pl-2 md:pl-4">
+              <img className="w-10 md:w-12" src={iconUser} />
+              <Text as="span" className="pl-2 md:pl-4" color="white">
                 Utilisateur
               </Text>
             </div>
           </div>
-          <div className="md:mt-10 px-4 mt-3">
-            <Text as="span" color="white" bold className="px-1">
+          <div className="mt-3 md:mt-10 px-4">
+            <Text as="span" bold className="px-1" color="white">
               Visibilité :
             </Text>
             <Text as="span" color="white">
               À l&apos;oeil nu
             </Text>
           </div>
-          <div className="md:mt-5 px-4 mt-3">
-            <Text as="span" color="white" bold className="px-1">
+          <div className="mt-3 md:mt-5 px-4">
+            <Text as="span" bold className="px-1" color="white">
               Orientation :
             </Text>
             <Text as="span" color="white">
               98°
             </Text>
           </div>
-          <div className="md:mt-5 px-4 mt-3">
-            <Text as="span" color="white" bold className="px-1">
+          <div className="mt-3 md:mt-5 px-4">
+            <Text as="span" bold className="px-1" color="white">
               Description :
             </Text>
             <Text as="span" color="white">
@@ -91,8 +91,8 @@ export default function ObservationPage(): JSX.Element {
         </div>
       </div>
       <Map
-        className="h-[calc(100vh-20rem)] md:h-[100vh] w-full"
         center={[49.2332, 1.813]}
+        className="h-[calc(100vh-20rem)] md:h-[100vh] w-full"
         noFly
         withoutNotificationCircle
       >
