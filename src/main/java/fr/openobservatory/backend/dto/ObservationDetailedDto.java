1a6c47f1889b9fa6ebf622727204b7abcb31c09f
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-17 08:59:29 +0100

    fix: ObservationDetailedDto's equals should call super.equals first

--- src/main/java/fr/openobservatory/backend/dto/ObservationDetailedDto.java
+++ src/main/java/fr/openobservatory/backend/dto/ObservationDetailedDto.java
@@ -1,8 +1,10 @@
 package fr.openobservatory.backend.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class ObservationDetailedDto extends ObservationDto {
   private boolean hasExpired;
   private int votes;
