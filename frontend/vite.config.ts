45c4d0a4c84167918fc7160b2d4bc7c87edfa6e1
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-17 15:54:54 +0100

    feat: PWA support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,29 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      manifest: {
+        name: 'Open Observatory',
+        short_name: 'Open Obs.',
+        description: "Open Observatory est une plateforme collaborative de partage d'observations d'objets célestes.",
+        background_color: '#101010',
+        theme_color: '#101010',
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+      registerType: 'autoUpdate',
+    }),
+  ],
 });
