abeaa475dc1d56d4a11007968358594dd1158f24
Thomas Hamelin <thomashamelin27@outlook.fr>
2023-03-20 14:20:43 +0100

    refacto

--- frontend/src/guards/AuthenticatedGuard.tsx
+++ frontend/src/guards/AuthenticatedGuard.tsx
@@ -5,9 +5,5 @@ import { useAuthentication } from '~/providers';
 export function AuthenticatedGuard({ children }: { children: React.ReactNode }): JSX.Element {
   const { user } = useAuthentication();
 
-  if (user) {
-    return <>{children}</>;
-  }
-
-  return <Navigate to="/login" replace />;
+  return user ? <>{children}</> : <Navigate to="/login" replace />;
 }
