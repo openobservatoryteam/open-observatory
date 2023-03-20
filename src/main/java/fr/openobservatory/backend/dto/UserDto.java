d5e4c4ba0cd55c154c4efe04df1bea44a0248a1a
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-20 23:22:16 +0100

    refactor: removed @JsonIgnore'd attributes

--- src/main/java/fr/openobservatory/backend/dto/UserDto.java
+++ src/main/java/fr/openobservatory/backend/dto/UserDto.java
@@ -1,22 +1,13 @@
 package fr.openobservatory.backend.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import fr.openobservatory.backend.entities.UserEntity;
-import java.time.Instant;
 import lombok.Data;
 
 @Data
 public class UserDto {
 
-  @JsonIgnore private Long id;
 
   private String username;
-
-  @JsonIgnore private String password;
-
   private String avatar;
-
   private UserEntity.Type type;
-
-  @JsonIgnore private Instant createdAt;
 }
