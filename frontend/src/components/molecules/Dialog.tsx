85b00b8fb86d80ec9d76e232b1ff048f8f5393b9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-21 17:52:58 +0100

    feat: close button on Dialog + fixed width on small viewports

--- frontend/src/components/molecules/Dialog.tsx
+++ frontend/src/components/molecules/Dialog.tsx
@@ -1,15 +1,19 @@
-import { Title } from '../atoms/Title';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { clsx } from 'clsx';
 import { useRef } from 'react';
 import { AriaDialogProps, useDialog } from 'react-aria';
 
+import { Button, Title } from '~/components';
+
 type DialogProps = AriaDialogProps & {
   children: React.ReactNode;
-  title: string;
   className?: string;
+  onClose?: () => unknown;
+  title: string;
 };
 
-function Dialog({ children, title, className, ...props }: DialogProps) {
+function Dialog({ children, className, onClose, title, ...props }: DialogProps) {
   const ref = useRef(null);
   const { dialogProps, titleProps } = useDialog(
     {
@@ -21,11 +25,19 @@ function Dialog({ children, title, className, ...props }: DialogProps) {
   return (
     <div
       {...dialogProps}
-      className={clsx('text-white bg-[#333C47] rounded-2xl w-1/2', className)}
+      className={clsx(
+        'text-white bg-[#333C47] rounded-2xl w-[95%] sm:w-[90%] md:w-[85%] lg:w-[75%] xl:w-[50%] relative',
+        className,
+      )}
       ref={ref}
       style={{ padding: 30 }}
     >
-      <Title as="h2" {...titleProps} style={{ marginTop: 0, paddingBottom: 30 }} centered>
+      {onClose && (
+        <Button className="absolute h-8 w-8" onClick={onClose} rounded>
+          <FontAwesomeIcon color="black" icon={faClose} size="2x" />
+        </Button>
+      )}
+      <Title as="h2" centered style={{ marginBottom: 30 }} {...titleProps}>
         {title}
       </Title>
       {children}
