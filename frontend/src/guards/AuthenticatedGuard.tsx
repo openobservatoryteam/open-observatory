3ccff25fb35ecd4c0e50afd2be527dd56073f21f
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-28 09:57:05 +0200

    fix: wait for API response to determine whether authenticated or not

--- frontend/src/guards/AuthenticatedGuard.tsx
+++ frontend/src/guards/AuthenticatedGuard.tsx
@@ -2,8 +2,8 @@ import { Navigate } from '@tanstack/react-location';
 
 import { useAuthentication } from '~/providers';
 
-export function AuthenticatedGuard({ children }: { children: React.ReactNode }): JSX.Element {
-  const { user } = useAuthentication();
-
+export function AuthenticatedGuard({ children }: { children: React.ReactNode }) {
+  const { isLoading, user } = useAuthentication();
+  if (isLoading) return null;
   return user ? <>{children}</> : <Navigate to="/login" replace />;
 }
