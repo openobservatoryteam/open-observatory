67ec1f5375bdc620059837f0376a5194fd9831b5
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-28 10:07:44 +0200

    refactor(DatePicker): remove custom calendar icon

--- frontend/src/components/atoms/DatePicker.tsx
+++ frontend/src/components/atoms/DatePicker.tsx
@@ -1,5 +1,3 @@
-import { faCalendar } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
 import { ComponentPropsWithoutRef, ForwardedRef, ReactNode, forwardRef } from 'react';
 
@@ -8,10 +6,12 @@ type DatePickerProps = ComponentPropsWithoutRef<'input'> & { errorMessage?: Reac
 function DatePicker({ className, errorMessage, ...props }: DatePickerProps, ref?: ForwardedRef<HTMLInputElement>) {
   return (
     <div>
-      <div className={clsx('bg-white flex px-4 py-2 rounded-3xl text-black w-full', className)}>
-        <input className="bg-inherit mr-auto outline-none w-full" ref={ref} type="datetime-local" {...props} />
-        <FontAwesomeIcon className="my-auto mr-1" icon={faCalendar} />
-      </div>
+      <input
+        className={clsx('bg-white flex mr-auto px-4 py-2 outline-none rounded-3xl text-black w-full', className)}
+        ref={ref}
+        type="datetime-local"
+        {...props}
+      />
       {errorMessage && <p className="text-center text-red-500">{errorMessage}</p>}
     </div>
   );
