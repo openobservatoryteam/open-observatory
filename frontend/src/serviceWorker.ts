17a1312c3729ee7c852e0936e8eef9816159a45c
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-03-29 14:37:16 +0200

    fix: fix service worker build

--- frontend/src/serviceWorker.ts
+++ frontend/src/serviceWorker.ts
@@ -1,30 +1,33 @@
+/// <reference lib="ES2015" />
 /// <reference lib="webworker" />
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const worker = self as unknown as ServiceWorkerGlobalScope;
+import { precacheAndRoute } from 'workbox-precaching';
 
-worker.addEventListener('activate', () => worker.clients.claim());
-worker.addEventListener('install', () => worker.skipWaiting());
+declare const self: ServiceWorkerGlobalScope;
+
+precacheAndRoute(self.__WB_MANIFEST ?? []);
+
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('install', () => self.skipWaiting());
 
 type NotificationPayload = {
+  icon?: string;
   link?: string;
   message: string;
 };
 
-worker.addEventListener('push', (event) => {
+self.addEventListener('push', (event) => {
   if (event.data === null) return;
-  const { link, message } = event.data.json() as NotificationPayload;
-  console.log('Reçu un message: ' + message);
-  worker.registration.showNotification('Open Observatory', {
+  const { icon, link, message } = event.data.json() as NotificationPayload;
+  self.registration.showNotification('Open Observatory', {
     body: message,
-    data: { link },
+    data: { icon, link },
     icon: '/assets/logo.png',
   });
 });
 
-if ('openWindow' in worker.clients) {
-  worker.addEventListener('notificationclick', (event) => {
+if ('openWindow' in self.clients) {
+  self.addEventListener('notificationclick', (event) => {
     const { notification } = event;
-    if (notification.data.link) worker.clients.openWindow(notification.data.link);
+    if (notification.data.link) self.clients.openWindow(notification.data.link);
   });
 }
