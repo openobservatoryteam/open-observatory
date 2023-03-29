8e690b27764517e52eb52bb9576d0b58d71ac447
Thomas Hamelin <thomashamelin27@outlook.fr>
2023-03-29 19:46:38 +0200

    translation about us

--- frontend/src/routes/AboutPage.tsx
+++ frontend/src/routes/AboutPage.tsx
@@ -1,16 +1,19 @@
+import { useTranslation } from 'react-i18next';
+
 import { Text } from '~/components';
 import { Footer, Header } from '~/layout';
 
 function AboutPage() {
+  const { t } = useTranslation();
   return (
     <>
       <Header className="h-16 my-1" />
       <div className="flex flex-col items-center">
         <Text as="p" centered className="mt-14 md:w-1/2 w-3/4">
-          Open Observatory est une application permettant de partager des observations d’objets célestes
+          {t('common.descriptionOpenObservatory')}
         </Text>
         <Text as="p" centered className="mt-16 md:w-1/2 w-3/4">
-          Si vous avez des questions, vous pouvez contacter le support à l’adresse suivante :
+          {t('common.questions')}
         </Text>
         <Text as="span" centered className="mt-16 md:w-1/2 w-3/4">
           support.openobservatory@gmail.com
