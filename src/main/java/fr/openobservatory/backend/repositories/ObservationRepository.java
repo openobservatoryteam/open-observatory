491fee4f7730e475469307a9789b3c892623829b
Coucoba <lucas.ficker@gmail.com>
2023-03-30 18:43:59 +0200

    add observer achievement

--- src/main/java/fr/openobservatory/backend/repositories/ObservationRepository.java
+++ src/main/java/fr/openobservatory/backend/repositories/ObservationRepository.java
@@ -3,6 +3,8 @@ package fr.openobservatory.backend.repositories;
 import fr.openobservatory.backend.entities.ObservationEntity;
 import fr.openobservatory.backend.entities.UserEntity;
 import java.util.Collection;
+import java.util.Set;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Repository;
 public interface ObservationRepository extends JpaRepository<ObservationEntity, Long> {
 
   Page<ObservationEntity> findAllByAuthor(UserEntity user, Pageable pageable);
+  Set<ObservationEntity> findAllByAuthor(UserEntity user);
 
   @Query(
       "FROM ObservationEntity o WHERE (o.latitude BETWEEN :latX AND :latY) AND (o.longitude BETWEEN :lngX AND :lngY) AND (CURRENT_TIMESTAMP - o.createdAt) < MAKE_INTERVAL(0, 0, 0, 0, o.celestialBody.validityTime)")
