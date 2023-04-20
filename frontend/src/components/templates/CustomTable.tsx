807c7f20f74d9863af2c748782e7999138844155
Thomas Hamelin <thomashamelin27@outlook.fr>
2023-04-20 19:40:15 +0200

    add display description on hover

--- frontend/src/components/templates/CustomTable.tsx
+++ frontend/src/components/templates/CustomTable.tsx
@@ -1,9 +1,9 @@
+import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
-import { ReactElement, ReactNode } from 'react';
-import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ReactNode } from 'react';
 
-import { Text, Button } from '~/components';
+import { Button, Text } from '~/components';
 
 export type ColumnsProps<T> = {
   name: string;
@@ -11,13 +11,13 @@ export type ColumnsProps<T> = {
 };
 
 export type OtherAction<T> = {
-  element: () => JSX.Element
-}
+  element: (obj: T) => JSX.Element;
+};
 
 export type CustomActionProps<T> = {
-  edit: { onEdit: (obj: T) => void; };
-  delete: { onDelete: (obj: T) => void; };
-  other?: OtherAction<T>[]
+  edit: { onEdit: (obj: T) => void };
+  delete: { onDelete: (obj: T) => void };
+  other?: OtherAction<T>[];
 };
 
 interface CustomTableProps<T> {
@@ -28,11 +28,19 @@ interface CustomTableProps<T> {
   customsAction?: CustomActionProps<T>;
   className?: string;
   onPageChange: (n: number) => void;
-  onItemsPerPageChange: (n : number) => void;
+  onItemsPerPageChange: (n: number) => void;
 }
 
-export function CustomTable<T>({ data, customsAction, columns, page, pageCount, className, onItemsPerPageChange, onPageChange }: CustomTableProps<T>) {
-
+export function CustomTable<T>({
+  data,
+  customsAction,
+  columns,
+  page,
+  pageCount,
+  className,
+  onItemsPerPageChange,
+  onPageChange,
+}: CustomTableProps<T>) {
   return (
     <div className={clsx(className)}>
       <table className="w-full">
@@ -40,14 +48,14 @@ export function CustomTable<T>({ data, customsAction, columns, page, pageCount, 
           <tr className="border-b-2 border-b-white pb-3 flex justify-between">
             {columns.map((e, index) => (
               <th key={index}>
-                <Text centered bold className='w-32'>
+                <Text centered bold className="w-32">
                   {e.name}
                 </Text>
               </th>
             ))}
             {customsAction && (
               <th>
-              <Text centered bold className='w-32'>
+                <Text centered bold className="w-32">
                   Actions
                 </Text>
               </th>
@@ -56,47 +64,69 @@ export function CustomTable<T>({ data, customsAction, columns, page, pageCount, 
         </thead>
         <tbody>
           {data.map((d, index) => (
-            <tr key={index} className='border-b-2 border-b-white py-3 flex justify-between items-center'>
+            <tr key={index} className="border-b-2 border-b-white py-3 flex justify-between items-center">
               {columns.map((e, index) => (
-                <td key={e.name + index} className='w-32 flex justify-center'>
+                <td key={e.name + index} className="w-32 flex justify-center">
                   {e.render(d)}
                 </td>
               ))}
               {customsAction && (
-                <td className='flex items-center justify-end gap-x-1 w-32'>
-                  <FontAwesomeIcon color="white" icon={faPen} onClick={() => customsAction.edit.onEdit(d)} className='p-2 rounded-full hover:cursor-pointer hover:bg-slate-400' />
-                  <FontAwesomeIcon icon={faTrash} onClick={() => customsAction.delete.onDelete} className='text-red-500 p-2 rounded-full hover:cursor-pointer hover:bg-slate-400' />
-                  {customsAction.other?.map(a => (a.element()))}
+                <td className="flex items-center justify-end gap-x-1 w-32">
+                  <FontAwesomeIcon
+                    color="white"
+                    icon={faPen}
+                    onClick={() => customsAction.edit.onEdit(d)}
+                    className="p-2 rounded-full hover:cursor-pointer hover:bg-slate-400"
+                  />
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    onClick={() => customsAction.delete.onDelete}
+                    className="text-red-500 p-2 rounded-full hover:cursor-pointer hover:bg-slate-400"
+                  />
+                  {customsAction.other?.map((a) => a.element(d))}
                 </td>
               )}
             </tr>
           ))}
         </tbody>
       </table>
-      <div className='flex justify-end items-center gap-x-5 mt-5'>
+      <div className="flex justify-end items-center gap-x-5 mt-5">
         {/* Sélection d'item par page */}
         <div>
-            <select onChange={(value) => onItemsPerPageChange(parseInt(value.currentTarget.value))} className='bg-transparent text-white border-white border-2 p-1 rounded-2xl'>
-              <option value={10} selected className='text-black'>10</option>
-              <option value={20} className='text-black'>20</option>
-              <option value={30} className='text-black'>30</option>
-              <option value={50} className='text-black'>50</option>
-              <option value={100} className='text-black'>100</option>
-            </select>
+          <select
+            onChange={(value) => onItemsPerPageChange(parseInt(value.currentTarget.value))}
+            className="bg-transparent text-white border-white border-2 p-1 rounded-2xl"
+          >
+            <option value={10} selected className="text-black">
+              10
+            </option>
+            <option value={20} className="text-black">
+              20
+            </option>
+            <option value={30} className="text-black">
+              30
+            </option>
+            <option value={50} className="text-black">
+              50
+            </option>
+            <option value={100} className="text-black">
+              100
+            </option>
+          </select>
         </div>
         {/* Sélection de la page */}
-        <div className='flex gap-2'>
-        {[...Array(pageCount)].map((_, n) => (
-        <Button
-          color={page === n ? 'white' : 'transparent'}
-          className={clsx('px-4 py-1', page === n ? 'text-black' : 'text-white', 'rounded-full')}
-          key={`pagination-${n}`}
-          onPress={() => (page === n ? undefined : onPageChange(n))}
-          rounded
-        >
-          {n + 1}
-        </Button>
-      ))}
+        <div className="flex gap-2">
+          {[...Array(pageCount)].map((_, n) => (
+            <Button
+              color={page === n ? 'white' : 'transparent'}
+              className={clsx('px-4 py-1', page === n ? 'text-black' : 'text-white', 'rounded-full')}
+              key={`pagination-${n}`}
+              onPress={() => (page === n ? undefined : onPageChange(n))}
+              rounded
+            >
+              {n + 1}
+            </Button>
+          ))}
         </div>
       </div>
     </div>
--- frontend/src/routes/UserAdminPage.tsx
+++ frontend/src/routes/UserAdminPage.tsx
@@ -1,10 +1,10 @@
+import { faShieldHalved, faUserShield } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useTranslation } from 'react-i18next';
-import { faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
 import { User } from '~/api';
 import iconUser from '~/assets/png/icon-user.png';
-import { AsideAdmin, ColumnsProps, CustomTable, Title, Text, CustomActionProps } from '~/components';
+import { AsideAdmin, ColumnsProps, CustomActionProps, CustomTable, Text, Title } from '~/components';
 
 function UserAdminPage() {
   const { t } = useTranslation();
@@ -12,7 +12,7 @@ function UserAdminPage() {
   const columns: ColumnsProps<User>[] = [
     {
       name: 'Avatar',
-      render: (obj: User) => <img src={obj.avatar ?? iconUser} className='w-10 h-10'/>,
+      render: (obj: User) => <img src={obj.avatar ?? iconUser} className="w-10 h-10" />,
     },
     {
       name: 'Username',
@@ -20,45 +20,72 @@ function UserAdminPage() {
     },
   ];
 
-  const userData : User[] = [
+  const userData: User[] = [
     {
-      username: "TOTO",
-      type: "USER",
+      username: 'TOTO',
+      type: 'USER',
       notificationsEnabled: true,
       public: true,
-      radius: 25
+      radius: 25,
     },
     {
-      username: "LULULU",
-      type: "USER",
+      username: 'LULULU',
+      type: 'ADMIN',
       notificationsEnabled: true,
       public: true,
-      radius: 25
+      radius: 25,
     },
     {
-      username: "KEKE",
-      type: "USER",
+      username: 'KEKE',
+      type: 'USER',
       notificationsEnabled: true,
       public: true,
-      radius: 25
-    }
-  ]
+      radius: 25,
+    },
+  ];
 
-  const customAction : CustomActionProps<User> = {
-    edit : {
-      onEdit : (obj) => {
-          console.log(obj.username)
+  const customAction: CustomActionProps<User> = {
+    edit: {
+      onEdit: (obj) => {
+        console.log(obj.username);
       },
     },
-    delete : {
+    delete: {
       onDelete: (obj) => {
-          console.log(obj.username)
+        console.log(obj.username);
       },
     },
-    other : [
-      { element : () => <FontAwesomeIcon icon={faShieldHalved} color="white" className='p-2 rounded-full hover:cursor-pointer hover:bg-slate-400'/>}
-    ]
-  }
+    other: [
+      {
+        element: (obj: User) => (
+          <>
+            {obj.type !== 'ADMIN' && (
+              <span className="tooltip">
+                <FontAwesomeIcon
+                  icon={faShieldHalved}
+                  color="white"
+                  className="p-2 rounded-full hover:cursor-pointer hover:bg-slate-400 w-5 tooltip"
+                  onClick={() => console.log(obj.username)}
+                />
+                <span className="tooltiptext">{t('common.user')}</span>
+              </span>
+            )}
+            {obj.type === 'ADMIN' && (
+              <span className="tooltip">
+                <FontAwesomeIcon
+                  icon={faUserShield}
+                  color="white"
+                  className="p-2 rounded-full hover:cursor-pointer hover:bg-slate-400 w-5"
+                  onClick={() => console.log(obj.username)}
+                />
+                <span className="tooltiptext">{t('common.admin')}</span>
+              </span>
+            )}
+          </>
+        ),
+      },
+    ],
+  };
 
   return (
     <div className="flex">
@@ -72,7 +99,7 @@ function UserAdminPage() {
           columns={columns}
           page={0}
           pageCount={2}
-          onItemsPerPageChange={(a) => console.log(a) }
+          onItemsPerPageChange={(a) => console.log(a)}
           onPageChange={(a) => console.log(a)}
           customsAction={customAction}
           className="w-3/4 bg-slate-500 mx-auto p-5 rounded-2xl"
