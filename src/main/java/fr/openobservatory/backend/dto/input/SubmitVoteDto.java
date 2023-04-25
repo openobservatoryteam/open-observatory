849d930873503abe089da13e9beefc88cf6546ce
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-04-25 12:02:14 +0200

    fix: add default case for SubmitVoteDto

--- src/main/java/fr/openobservatory/backend/dto/input/SubmitVoteDto.java
+++ src/main/java/fr/openobservatory/backend/dto/input/SubmitVoteDto.java
@@ -3,12 +3,14 @@ package fr.openobservatory.backend.dto.input;
 import fr.openobservatory.backend.entities.ObservationVoteEntity.VoteType;
 import fr.openobservatory.backend.validation.EnumValue;
 import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Data;
 
 @Builder
 @Data
 public class SubmitVoteDto {
 
+  @Default
   @EnumValue(message = "vote.invalid", value = VoteType.class)
-  private String vote;
+  private String vote = null;
 }
