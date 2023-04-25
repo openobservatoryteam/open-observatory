9405a9a4e2492c94859a90630b82f2f2d067a144
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-04-25 12:05:03 +0200

    fix: JsonParse error (unknown reason why)

--- src/main/java/fr/openobservatory/backend/dto/input/SubmitVoteDto.java
+++ src/main/java/fr/openobservatory/backend/dto/input/SubmitVoteDto.java
@@ -2,15 +2,17 @@ package fr.openobservatory.backend.dto.input;
 
 import fr.openobservatory.backend.entities.ObservationVoteEntity.VoteType;
 import fr.openobservatory.backend.validation.EnumValue;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Builder.Default;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class SubmitVoteDto {
 
-  @Default
   @EnumValue(message = "vote.invalid", value = VoteType.class)
-  private String vote = null;
+  private String vote;
 }
